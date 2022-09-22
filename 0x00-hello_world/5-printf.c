4ed56cd4c283e6bc9017757f16837737841ff66d
lawalkeyd <lawalkeyd@gmail.com>
2022-09-22 13:45:23 -0700

    puts 5-printd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entrty point
+ *
+ * Description: Practicing put
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char text[] = "with proper grammar, but the outcome is a piece of art,";
+
+	puts(text);
+	return (0);
+
+}
+
