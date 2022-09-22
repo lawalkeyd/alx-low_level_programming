e428fadf6cfef3c7224a9e197eb2302b06b1d001
lawalkeyd <lawalkeyd@gmail.com>
2022-09-22 13:32:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+/*
+ * main - entrty point
+ *
+ * Description: Practicing put
+ * Return: always 0
+ */
 int main(void)
 {
+
 	char text[] = "\"Programming is like building a multilingual puzzle";
 	puts(text);
+
 	return (0);
+
 }
 
