cd829fd65e2dd1b100643c247e6405b46898dfd5
lawalkeyd <lawalkeyd@gmail.com>
2022-09-23 05:58:47 -0700

    Fix betty errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,22 +3,24 @@
 # include <stdio.h>
 
 /**
- * betty syle doc for main
  * main - this is the main function
- * Returns: returns 0 (default)
+ * Description: Checks if a value a rnadom value is positive or negative
+ *
+ * Return: return 0 (default)
  */
 int main(void)
 {
-	// this is the n declaration
+	/* this is the n declaration */
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		printf("%d is negative\n", n);		
-       	else if (n == 0{
+		printf("%d is negative\n", n);
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is positive\n", n);
-	return(0);
+	return (0);
 }
+
