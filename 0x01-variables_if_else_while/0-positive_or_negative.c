5907af5d52d4490076a1cf0a4a8bd74e8fcef767
lawalkeyd <lawalkeyd@gmail.com>
2022-09-23 05:23:50 -0700

    add readme

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,21 @@
 
 /**
  * betty syle doc for main
+ * main - this is the main function
+ * Returns: returns 0 (default)
  */
 int main(void)
 {
+	// this is the n declaration
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n < 0)
-	{
 		printf("%d is negative\n", n);		
-	} else if (n == 0){
+       	else if (n == 0{
 		printf("%d is zero\n", n);
-	} else {
+	else
 		printf("%d is positive\n", n);
-	}
 	return(0);
 }
